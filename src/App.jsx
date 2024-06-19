088a3aa3437a5ab6fd8a03e42d50a2bf0c503a99
nextb <HDNEXT2@GMAIL.COM>
2024-06-19 20:30:45 +0300

    add text about me

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="bg-stone-900 w-full h-screen">
       <div className="flex flex-col justify-center ">
-        <h1 className=" text-white mx-auto pt-12 text-2xl font-semibold tracking-tight">
+        <h1 className=" text-white mx-auto pt-12 text-2xl font-semibold tracking-tight ">
           Vladislav
         </h1>
         <h4 className=" text-neutral-400 mx-auto pt-2 text-m font-semibold tracking-tight">
@@ -17,16 +17,28 @@ function App() {
         
       </div> */}
 
-      <div className=" pt-24 ">
-        <Avatar className="size-24 mx-auto justify-center">
-          <AvatarImage src="https://avatars.githubusercontent.com/u/25897342?v=4" />
-          <AvatarFallback>vlad</AvatarFallback>
-        </Avatar>
-        <p className=" text-white max-w-40 float-right mr-40 ">
+      <div className="pt-24 flex justify-center">
+        <p className=" text-white max-w-44 float-right mr-40 font-semibold">
           Jokester began sneaking into the castle in the middle of the night and
           leaving jokes all over the place: under the king's pillow, in his
           soup, even in the royal toilet. The king was furious, but he couldn't
           seem to stop Jokester.
+        </p>
+        <Avatar className="size-24  justify-center">
+          <AvatarImage src="https://avatars.githubusercontent.com/u/25897342?v=4" />
+          <AvatarFallback>vlad</AvatarFallback>
+        </Avatar>
+        <p className=" text-white max-w-40 float-right ml-40 font-semibold " >
+          <span className="text-cyan-300 text-shadow-lg">
+            &lt;education&gt;
+              <span className="text-white">Belgorod state university (BSU)</span>
+            &lt;/education&gt;
+          </span>
+          <span className="text-yellow-300 text-shadow-lg">
+            &lt;experience&gt;
+              <span className="text-white"><br/>1+ year<br/></span>
+            &lt;/experience&gt;
+          </span>
         </p>
       </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,18 @@ module.exports = {
   ],
   prefix: "",
   theme: {
+    fontFamily: {
+      'Keleti': ['Keleti', 'Regular'],
+      'PIXY': ['PIXY', 'sans-serif'],
+      'CHNOPixel': ['CHNOPixel', 'Regular'],
+    },
     container: {
       center: true,
       padding: "2rem",
       screens: {
         "2xl": "1400px",
       },
+      
     },
     extend: {
       colors: {
